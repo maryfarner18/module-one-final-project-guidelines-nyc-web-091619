890a969dc608ab538626da065b850f8d0fc1d2fd
maryfarner18 <maryfarner@gmail.com>
2019-09-30 14:23:12 -0400

    update with main structure

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 require 'bundler'
+require "sinatra/activerecord"
+require 'pry'
+
 Bundler.require
+
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
