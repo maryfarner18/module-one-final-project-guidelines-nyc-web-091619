607d487af5d7ffb746e50361487dafea7c7cf5ab
maryfarner18 <maryfarner@gmail.com>
2019-10-03 16:34:28 -0400

    Added time to pretty walks

--- app/helpers.rb
+++ app/helpers.rb
@@ -13,6 +13,30 @@ def string_to_datetime(date, time, ampm)
     date_and_time = Time.utc(date[0].to_i, date[1].to_i, date[2].to_i, hours, time[1].to_i)
 end
 
+def mil_to_ampm(hours, minutes)
+    ampm = ''
+    if hours >= 12 
+        ampm = "PM"
+    else
+        ampm = "AM"
+    end
+
+    if hours > 12
+        hours -= 12
+    end
+
+    if hours == 0
+        hours = 12
+    end
+
+    if minutes == 0
+        minutes = "00"
+    end
+    
+    "#{hours}:#{minutes} #{ampm}"
+
+end
+
 #prints all the walks in walk_array like
 #   #14: 30 minutes walk for Oberon on October 10, 2019
 def pretty_walks(walk_array)
@@ -21,7 +45,9 @@ def pretty_walks(walk_array)
         month = walk.date_and_time.month
         day = walk.date_and_time.day
         year = walk.date_and_time.year
-        "##{walk.id}: #{walk.length} minute walk for #{walk.dog.name} on #{Date::MONTHNAMES[month]} #{day}, #{year}"
+        hours = walk.date_and_time.hour
+        minutes = walk.date_and_time.min
+        "##{walk.id}: #{walk.length} minute walk for #{walk.dog.name} on #{Date::MONTHNAMES[month]} #{day}, #{year} at #{mil_to_ampm(hours, minutes)}"
     end
 end
 
